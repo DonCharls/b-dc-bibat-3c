40175e9ca756c16ea703a56ee7a74c3ab694dec0
DonCharls <qubeasia12345@gmail.com>
2024-08-17 17:41:18 +0800

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PostController } from './post/post.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AppController],
+  controllers: [AppController, PostController],
   providers: [AppService],
 })
 export class AppModule {}
